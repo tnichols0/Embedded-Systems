894db1c17fad893b35c31957d34604d427fc4a67
tnichols0 <146337457+tnichols0@users.noreply.github.com>
2023-10-12 15:32:41 +0100

    Update main.cpp

--- Tasks/Task-104-TrafficLights/main.cpp
+++ Tasks/Task-104-TrafficLights/main.cpp
@@ -1,4 +1,6 @@
 #include "uop_msb.h"
+#include <iterator>
+#include <type_traits>
 using namespace uop_msb;
 
 #define WAIT_TIME_MS 500 
@@ -9,29 +11,46 @@ DigitalOut green(TRAF_GRN1_PIN,0);
 
 LCD_16X2_DISPLAY lcd;
 
+
 int main()
 {
+ while(true) {
+
+    amber = 0;
+    red = 1;
+    lcd.cls();
     lcd.puts("RED");
-    wait_us(1000000);
+    wait_us(10000000);
 
     amber = 1;
     lcd.cls();
     lcd.puts("Amber");
-    wait_us(1000000);
+    wait_us(2000000);
 
     green = 1;
+    amber = 0;
+    red = 0;
     lcd.cls();
     lcd.puts("Green");    
-    wait_us(1000000);
+    wait_us(10000000);
 
+    amber = 1;
+    green = 0;
     lcd.cls();
-    lcd.puts("TASK-104");
+    lcd.puts("Amber");
+    wait_us(2000000);
 
-    while (true)
-    {
-        red = !red;
-        amber = !amber;
-        green = !green;
-        wait_us(WAIT_TIME_MS * 1000);
-    }
+
+ }
+
+   // lcd.cls();
+   // lcd.puts("TASK-104");
+
+   // while (true)
+    //{
+      //  red = !red;
+      //  amber = !amber;
+       // green = !green;
+     //   wait_us(WAIT_TIME_MS * 1000);
+   // }
 }
