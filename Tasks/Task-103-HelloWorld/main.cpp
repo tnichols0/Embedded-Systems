2e671dc09708681870c427f433b968a63f590942
tnichols0 <146337457+tnichols0@users.noreply.github.com>
2023-10-12 15:32:37 +0100

    Update main.cpp

--- Tasks/Task-103-HelloWorld/main.cpp
+++ Tasks/Task-103-HelloWorld/main.cpp
@@ -29,24 +29,30 @@ int main()
     // Write to LCD using the LCD_16X2_DISPLAY driver class
     lcd.printf("Hello World");
     lcd.locate(1, 0);   //Row 1, Col 0
-    lcd.printf("ELEC143");
+    lcd.printf("PROJ100");
 
     // This is a variable (not an oject as it has no functions) that stores a whole number (integer) in memory
     // (I used the keyword volatile to force it to use memory... long story and one for later)
     volatile int counter = 0;
 
-    while (true)
+    while (counter < 10)
     {
         //Toggle the LED
         ledBlue = !ledBlue;
-
-        //Add 1 to the counter "variable"
-        counter = counter + 1;
 
         //Display in the terminal
         printf("Count: %d\n", counter);
 
         //Wait
         wait_us(WAIT_TIME_MS * 1000);
+
+        //Add 1 to the counter "variable"
+        counter = counter + 1;        
     }
+
+    ledRed = 1;
+    ledGreen = 1;
+    ledBlue = 1;
+
+    wait_us(osWaitForever);
 }
