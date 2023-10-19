15739229e244e2e92841f52fc934fedc42a52167
tnichols0 <146337457+tnichols0@users.noreply.github.com>
2023-10-19 14:53:19 +0100

    Update main.cpp

--- Tasks/Task-112-Terminalnput/main.cpp
+++ Tasks/Task-112-Terminalnput/main.cpp
@@ -22,20 +22,27 @@ int main()
     c2 = getchar();
     printf("You entered character %c which has the ASCII code %d\n", c2, c2);
 
-    //scanf
-    printf("\n\nTo set the delay (in ms), type in an integer number and press return\n");
-    int delay_ms;
-    int parsed = scanf("%d", &delay_ms);
+    //Lights on
+    printf("\n\nTo set the delay the lights stay on for (in ms), type in an integer number and press return\n");
+    int delay_ms_on;
+    int parsedon = scanf("%d", &delay_ms_on);
     
-    printf("You entered %d correct integer values. The value was %dms\n", parsed, delay_ms);
+    printf("You entered %d correct integer values. The value was %dms\n", parsedon, delay_ms_on);
+
+    //Lights off
+    printf("\n\nTo set the delay the lights stay off for (in ms), type in an integer number and press return\n");
+    int delay_ms_off;
+    int parsedoff = scanf("%d", &delay_ms_off);
+    
+    printf("You entered %d correct integer values. The value was %dms\n", parsedoff, delay_ms_off);
 
     // Stop
-    while (parsed > 0) {
-        wait_us(delay_ms * 1000);
+    while (parsedon > 0) {
+        wait_us(delay_ms_on * 1000);
         led1 = 1;
         led2 = 1;
         led3 = 1;
-        wait_us(delay_ms * 1000);
+        wait_us(delay_ms_off * 1000);
         led1 = 0;
         led2 = 0;
         led3 = 0;        
