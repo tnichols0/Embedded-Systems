9896ee4e94886c986294524389be59c80c177613
tnichols0 <146337457+tnichols0@users.noreply.github.com>
2023-11-02 12:45:23 +0000

    Update main.cpp

--- Tasks/Task-115-BusOut/main.cpp
+++ Tasks/Task-115-BusOut/main.cpp
@@ -6,7 +6,7 @@
 // DigitalOut yellowLED(PC_3,1);
 // DigitalOut greenLED(PC_6,1);
 
-BusOut lights(PC_2, PC_3, PC_6);
+BusOut lights(PC_2, PC_3, PC_6, PB_0, PB_7, PB_14);
 //BusOut lights(PC_2, PC_3, PC_6, PB_0, PB_7, PB_14);
 
 int main()
@@ -16,14 +16,14 @@ int main()
 
     while (true)
     {
-        int count = 0;
-        while (count <= 7) 
+        int count = 63;
+        while (count >=0) 
         {
             printf("count = %d\n", count);
             lights = count;
-            wait_us(1000000);
+            wait_us(250000);
 
-            count = count + 1;
+            count = count - 1;
         }
     }
 }
