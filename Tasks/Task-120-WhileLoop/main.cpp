7bdffc2af453498e244bff49b9ef1e4b3b639837
tnichols0 <146337457+tnichols0@users.noreply.github.com>
2023-11-02 15:04:09 +0000

    Update main.cpp

--- Tasks/Task-120-WhileLoop/main.cpp
+++ Tasks/Task-120-WhileLoop/main.cpp
@@ -11,7 +11,7 @@ int main()
 
     printf("\n\nPress Return to Start\n\n");
 
-    while (hits < 1) {
+    while ((hits < 1) || (grade>100) || (grade<0) )  {
         //Clear out the serial port (keyboard)
         flushInputBuffer();
         //Prompt user
