6107b17f39ddd967bd0dd8e795e1acb93fe1ed31
tnichols0 <146337457+tnichols0@users.noreply.github.com>
2023-11-09 15:25:06 +0000

    Update main.cpp

--- Tasks/Task-127-BusIn/main.cpp
+++ Tasks/Task-127-BusIn/main.cpp
@@ -1,11 +1,7 @@
 #include "mbed.h"
 
-DigitalIn ButtonA(PG_0); //Button A
-DigitalIn ButtonB(PG_1); //Button B
-DigitalIn ButtonC(PG_2, PullDown); //Button C
-DigitalIn ButtonD(PG_3, PullDown); //Button D
-
-// BusIn bus(PG_0, PG_1, PG_2, PG_3);
+BusIn bus(PG_0, PG_1, PG_2, PG_3);
+int swVal;
 
 DigitalOut redLED(PC_2); //Red Traffic 1
 
@@ -15,24 +11,28 @@ int main()
     // Turn OFF the red LED
     redLED = 0;
 
-    // bus[2].mode(PinMode::PullDown);
-    // bus[3].mode(PinMode::PullDown);
+    bus[2].mode(PinMode::PullDown);
+    bus[3].mode(PinMode::PullDown);
     
     while (true) {
         
         //Wait for ALL buttons to be pressed
-        while ((ButtonA == 0) || (ButtonB == 0) || (ButtonC == 0) || (ButtonD == 0)) {
-            //Spin in a loop
-        }
+        do {
+            swVal = bus; 
+        }while(swVal<15);
+
+            
+        
 
         redLED = !redLED;
 
         //Wait for noise to settle
         wait_us(10000);
 
-        while ((ButtonA == 1) && (ButtonB == 0) && (ButtonC == 0) && (ButtonD == 0)) {
-            //Spin in a loop
-        }
+        do 
+        {
+            swVal = bus;
+        }while (swVal > 0);
 
         //Wait for noise to settle
         wait_us(10000);        
