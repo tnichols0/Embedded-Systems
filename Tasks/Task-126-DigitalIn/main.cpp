3d53180e1133d67c5b4c249c0036758d8132d22d
tnichols0 <146337457+tnichols0@users.noreply.github.com>
2023-11-09 12:56:14 +0000

    Update main.cpp

--- Tasks/Task-126-DigitalIn/main.cpp
+++ Tasks/Task-126-DigitalIn/main.cpp
@@ -1,12 +1,16 @@
 #include "mbed.h"
 
 DigitalIn ButtonA(PG_0); //Button A
+DigitalIn ButtonB(PG_1); //Button B
+DigitalIn ButtonC(PG_2, PinMode::PullDown); //Button C
+DigitalIn ButtonD(PG_3, PinMode::PullDown); //Button D
+
 DigitalOut redLED(PC_2); //Red Traffic 1
 
 // main() runs in its own thread in the OS
 int main()
 {
-    int btnA;
+    int btnA,btnB,btnC,btnD;
     // Turn OFF the red LED
     redLED = 0;
 
@@ -15,22 +19,28 @@ int main()
         // Wait for the button to be pressed
         do {
             btnA = ButtonA; //Read button A
-        } while (btnA == 0);
+            btnB = ButtonB; //Read button B
+            btnC = ButtonC; //Read button C
+            btnD = ButtonD; //Read button D
+        } while ((btnA == 0) || (btnB == 0) || (btnC == 0) || (btnD == 0));
 
 
         //Toggle the red LED
         redLED = !redLED;
 
         //Wait for noise to settle
-        wait_us(10000);
+        wait_us(100000);
 
         // Wait for the button to be released
         do {
             btnA = ButtonA; //Read button A
-        } while (btnA == 1);
+            btnB = ButtonB; //Read button B
+            btnC = ButtonC; //Read button C
+            btnD = ButtonD; //Read button D
+        } while ((btnA == 1) && (btnB == 1) && (btnC == 0) && (btnD == 0));
 
         //Wait for noise to settle
-        wait_us(10000);
+        wait_us(100000);
     }
 }
 
