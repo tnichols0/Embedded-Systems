d594229f3ee15b6431639efdd1c587e1053a935f
tnichols0 <146337457+tnichols0@users.noreply.github.com>
2023-11-16 12:42:39 +0000

    Update main.cpp

--- Tasks/Task-130-if-elseif-else/main.cpp
+++ Tasks/Task-130-if-elseif-else/main.cpp
@@ -1,11 +1,11 @@
 #include "uop_msb.h"
 using namespace uop_msb;
 
-DigitalIn ButtonA(PG_0); //Button A
-DigitalIn ButtonB(PG_1); //Button B
+ //Button B
 DigitalIn ButtonC(PG_2, PullDown); //Button C
 DigitalIn ButtonD(PG_3, PullDown); //Button D
-
+BusIn bus(PG_0, PG_1);
+int swVal=bus;
 DigitalOut redLED(TRAF_RED1_PIN);       //Red Traffic 1
 DigitalOut yellowLED(TRAF_YEL1_PIN);    //Yellow Traffic 1
 DigitalOut greenLED(TRAF_GRN1_PIN);     //Green Traffic 1
@@ -23,34 +23,38 @@ int main()
     //Update display
     disp = count;
 
-    int btnA = ButtonA;     //Local to the while-loop  
-    int btnB = ButtonB;
+    int swVal=bus;
 
     while (true) {
-        if ((btnA == 0) || (btnB == 0)) {
-        greenLED = 1;
-        wait_us(100000);
-        greenLED = 0;
-        wait_us(100000);
-        }
-        
+        swVal=bus;
+        greenLED=0;
         //Test Button A
-        if (btnA == 1) {
-            greenLED = 0;
+        if (bus[0] == 1) {
             redLED = !redLED;    //Toggle RED led
-            count = count + 1;            //Increment count
-            disp = count;       //Update display
+            count = count +((count<99)?1:0);            //Increment count
+            disp=count;       //Update display
         }
-
-        else if (btnB == 1) {
-            greenLED = 0;
-            redLED = !redLED;
-            count = count - 1;
-            disp = count;
+        if (bus[1]==1){
+            redLED=!redLED;
+            count=count-1;
+            disp=count;
         }
-    
+        if ((bus[0]==1) && (bus[1]==1)){
+            greenLED=1;
+            count=0;
+            disp=count;
+        }
+        if ( (count<0) || (count==99) ){
+            count=0;
+            disp=count;
+        }
+        if( (bus[0]==0) && (bus[1]==0)){
+            greenLED=1;
+        }
         // Slow it down a bit (and debounce the switches)
-        wait_us(100000);  
+        wait_us(100000);
+    
+        greenLED=0;   
     }
 }
 
